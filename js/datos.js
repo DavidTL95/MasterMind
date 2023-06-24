73c1c4020a4c16ff4e77db5a1718c43518a4d553
DavidTL95 <david.torrado95@gmail.com>
2023-06-24 20:59:05 +0200

    paso de parametros nombre de usuario, dificultad y colores guardados por el usuario a la pantalla de juego

--- js/datos.js
+++ js/datos.js
@@ -1,63 +1,122 @@
+
+//COGEMOS EL INPUT QUE ALBERGA EL NOMBRE.
 let nombre = document.getElementById("nombre");
 
-let mostrarNombre = document.getElementById("mostrarNombre");
-let mostrarColor1 = document.getElementById("mostrarColor1");
-
-let colorPicker = document.getElementById("colorPicker");
-
-let color1 = document.getElementById("color1");
-let color2 = document.getElementById("color2");
-let color3 = document.getElementById("color3");
-let color4 = document.getElementById("color4");
-let colores = []
-
-
-const dificultad = () => {
-
-    window.location.href = "../pages/dificultad.html"
-
+//FUNCIÓN GUARDAR NOMBRE EN SESSION STORAGE.
+const guardarNombre = () => {
+    sessionStorage.setItem("nombreUsuario", nombre.value)
 }
 
-colorPicker.addEventListener('change', (e) => {
+//FUNCIÓN GUARDAR DIFICULTAD.
+const guardarDificultad = (dificultad) => {
+    sessionStorage.setItem("dificultad", dificultad);
+}
 
-    if(color1.style.backgroundColor === ""){
+// let color1 = document.getElementById("color1");
+// let color2 = document.getElementById("color2");
+// let color3 = document.getElementById("color3");
+// let color4 = document.getElementById("color4");
 
-        color1.style.backgroundColor = e.target.value;
-        //colores.push(e.target.value);
-        
-    }else if(color2.style.backgroundColor === ""){
+//BOTÓN IR A LA PANTALLA DE JUEGO.
+let botonComenzar = document.getElementById("botonComenzar");
 
-        //colores.push(e.target.value);
-        color2.style.backgroundColor = e.target.value;
-
-    }else if(color3.style.backgroundColor === ""){
-
-        //colores.push(e.target.value);
-        color3.style.backgroundColor = e.target.value;
-    }else{
-        //colores.push(e.target.value);
-        color4.style.backgroundColor = e.target.value;
-    }
-})
-
-const guardarDatos = () => {
-
+//FUNCIÓN IR A LA PANTALLA DE JUEGO.
+const jugar = () => {
     if(nombre.value != ""){
-
-        sessionStorage.setItem("nombre", nombre.value);
-
-        sessionStorage.setItem("colores", colores.values);
-
-        window.location.href = "../pages/juego.html"
-
+        window.location.href = "../pages/juego.html";
     }else{
         nombre.style.borderColor = "red";
     }
-
 }
 
-mostrarNombre.innerHTML = sessionStorage.getItem("nombre")
-mostrarColor1.style.backgroundColor = sessionStorage.getItem(colores[0])
+//OBETNER LOS DIVS QUE ALBERGARÁN LOS COLORES + COLOR PICKER.
+
+let colores = document.getElementsByClassName('color');
+let colorPicker = document.getElementById('colorPicker');
+let coloresGuardados = [];
+
+//EVENTO OBTENER COLORES.
+
+colorPicker.addEventListener(('change'), (e) => {
+
+    // colores.array.forEach(element => {
+    //     element.style.backgroundColor = e.target.value;
+    // });
+
+    if(colores[0].style.backgroundColor === ""){
+
+        color1.style.backgroundColor = e.target.value;
+        coloresGuardados.push(e.target.value);
+        
+    }else if(colores[1].style.backgroundColor === ""){
+
+        color2.style.backgroundColor = e.target.value;
+        coloresGuardados.push(e.target.value);
+
+    }else if(colores[2].style.backgroundColor === ""){
+
+        colores[2].style.backgroundColor = e.target.value;
+        coloresGuardados.push(e.target.value);
+
+    }else if(colores[3].style.backgroundColor === ""){
+
+        colores[3].style.backgroundColor = e.target.value;
+        coloresGuardados.push(e.target.value);
+        
+        sessionStorage.setItem("colores", coloresGuardados)
+
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     if(nombre.value != ""){
+
+//         sessionStorage.setItem("nombre", nombre.value);
+
+//         let dif = document.querySelector(".btn-check").checked.value;
+
+//         sessionStorage.setItem("dificultad", dif);
+
+//         // let stringColores = JSON.stringify(colores);
+
+//         // sessionStorage.setItem("stringColores", colores.values);
+
+
+//     }else{
+//         nombre.style.borderColor = "red";
+//     }
 
 
 
@@ -68,7 +127,7 @@ mostrarColor1.style.backgroundColor = sessionStorage.getItem(colores[0])
 
 
 // const changeColours=() =>{
-//     let levelSelected = valorActivo = document.querySelector('input[name="radioButton"]:checked').value;
+//     let dificultad = document.querySelector('input[name="btnradio"]:checked').value;
 
 //     switch (levelSelected){
 //         case '1':
@@ -84,5 +143,5 @@ mostrarColor1.style.backgroundColor = sessionStorage.getItem(colores[0])
 //     coloursBoard.innerHTML=""
 //     for(let i=1;i<=colours;i++){
 
-//         coloursBoard.innerHTML+=`
-//         <div class="col-12 col-md-2 d-flex justify-content-ce
+//     coloursBoard.innerHTML+=`
+//     <div class="col-12 col-md-2 d-flex justify-content-ce
---
+++ js/juego.js
@@ -0,0 +1,23 @@
+
+let nombre = sessionStorage.getItem("nombreUsuario");
+
+let dificultad = sessionStorage.getItem("dificultad");
+
+let coloresEscogidos = sessionStorage.getItem("colores");
+
+let respuesta = [];
+
+let arrayBoss = [];
+
+// const coloresBoss = () => {
+//     arrayBoss.push(coloresEscogidos[Math.floor(Math.random() * coloresEscogidos.length)]);
+// }
+
+console.log(nombre)
+console.log(dificultad)
+console.log(coloresEscogidos)
+    // coloresEscogidos = sessionStorage.getItem("stringColores");
+    // let arrayColores = JSON.parse(coloresEscogidos);
+    // console.log(arrayColores);
+    // let dif = sessionStorage.getItem("dificultad")
+    // console.log(dif)
